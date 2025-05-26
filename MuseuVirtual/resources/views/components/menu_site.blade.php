35b2a666ca66b29f1dcbb22cc9e4f382bdb79933
Livia <vieiraleallivia@gmail.com>
2025-05-26 10:07:27 -0300

    Atualizando arquivos do front

--- MuseuVirtual/resources/views/components/menu_site.blade.php
+++ MuseuVirtual/resources/views/components/menu_site.blade.php
@@ -5,9 +5,9 @@
             <li><a href="#" class="p-5 pr-40 text-[#565851]">Museu Virtual ES</a></li>
             <li><a href="#" class="p-5 text-[#565851]">JAZIDAS</a></li>
             <li><a href="#" class="p-5 text-[#565851]">ROCHAS</a></li>
-            <li><a href="#" class="p-5 pr-40 text-[#565851]">MINERAIS</a></li>
-            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] mr-5 text-[#F1EEDD]">CADASTRAR</a></li>
-            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] text-[#F1EEDD]">LOGIN</a></li>
+            <li><a href="#" class="p-5 pr-40 text-[#565851] ">MINERAIS</a></li>
+            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] mr-5 text-[#F1EEDD] bg-[#ACB18E] hover:bg-[#A39D8C]">CADASTRAR</a></li>
+            <li><a href="#" class="p-2 pl-5 pr-5 rounded-full bg-[#ACB18E] text-[#F1EEDD] bg-[#ACB18E] hover:bg-[#A39D8C]">LOGIN</a></li>
         </ul>
     </nav>
 </header>
--- MuseuVirtual/resources/views/mineral.blade.php
+++ MuseuVirtual/resources/views/mineral.blade.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Mineral</title>
 </head>
-<body>
+
+<body class="bg-gradient-to-b from-[#ACB18E] from-0.5% via-[#73785C] via-5% to-[#363C27] to-20% ">
     <x-menu_site></x-menu_site>
-    
+    <x-rodape_site></x-rodape_site>
 </body>
+
 </html>
